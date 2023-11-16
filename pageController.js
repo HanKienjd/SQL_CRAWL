c61d45cc2a12bd7ca8048f59db9e50b8ce441921
kien.pham2 <kien.pham2@vndirect.com.vn>
2023-11-16 18:13:35 +0700

    pending task

--- pageController.js
+++ pageController.js
@@ -1,10 +1,55 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
+const csv = require('csv-parser');
+		
 async function scrapeAll(browserInstance){
 	let browser;
 	try{
 		browser = await browserInstance;
-		await pageScraper.scraper(browser);	
-		
+
+		// read data in file url.csv
+		const results = [];
+		await fs.createReadStream('url.csv')
+			.pipe(csv())
+			.on('data', (data) => results.push(data))
+			.on('end', () => {
+				results.forEach(async (item, index) => {
+					if (index < 5) {
+						let page = await browser.newPage();
+						await page.goto(`https://benhvienthucuc.vn${item.URL}`);
+						await page.waitForSelector('.container');
+						const result = await page.evaluate(() => {
+							const metaTags = document.getElementsByTagName('meta');
+							var keywords = [];
+							for (var i = 0; i < metaTags.length; i++) {
+        // Check if the meta tag has a 'name' attribute with a value of 'keywords'
+        if (metaTags[i].getAttribute('name') === 'keywords') {
+            // Get the content attribute of the meta tag
+            var keywordsContent = metaTags[i].getAttribute('content');
+            
+            // Split the content into an array of keywords
+            var keywordsArray = keywordsContent.split(',');
+            
+            // Trim any leading or trailing whitespaces from each keyword
+            keywordsArray = keywordsArray.map(function(keyword) {
+                return keyword.trim();
+            });
+						keywords.push(keywordsArray);
+            // Output the keywords
+        }
+    }
+							return keywords;
+						});
+						// save data to file
+						fs.writeFile('data.json', JSON.stringify(result), function (err) {
+							if (err) throw err;
+							console.log('Saved!');
+						});
+						// close page
+						await page.close();
+				}
+				});				
+		})
 	}
 	catch(err){
 		console.log("Could not resolve the browser instance => ", err);
--- pageScraper.js
+++ pageScraper.js
@@ -1,50 +1,55 @@
 const scraperObject = {
-	url: 'https://noithatthucung.com/danh-muc/san-pham/',
+  // url: 'https://benhvienthucuc.vn/khoa-kham-benh/khoa-tim-mach',
 	async scraper(browser){
 		let page = await browser.newPage();
-		console.log(`Navigating to ${this.url}...`);
 		await page.goto(this.url);
-    await page.waitForSelector('.shop-container');
-    let products =await page.evaluate(() => {
-      let getBox = document.querySelectorAll('.title-wrapper');
-      let product = [];
-      getBox.forEach((item) => {
-        product.push({
-          // @ts-ignore
-          title: item.querySelector('a').innerText,
-          // @ts-ignore
-          link: item.querySelector('a').href
-        });
-      });
-      
-      return product;
-    });
+    // await page.waitForSelector('.container');
+    await page.evaluate(() => {
+     // Function to extract keywords from the meta tags
+  let metaTags = document.getElementsByTagName('meta');
+  console.log("🚀 ~ file: pageScraper.js:12 ~ extractKeywords ~ metaTags:", metaTags)
+// function extractKeywords() {
+//     // Get all meta tags in the document
+  
+    
+//     // Loop through all meta tags
+//     // for (var i = 0; i < metaTags.length; i++) {
+//     //     // Check if the meta tag has a 'name' attribute with a value of 'keywords'
+//     //     if (metaTags[i].getAttribute('name') === 'keywords') {
+//     //         // Get the content attribute of the meta tag
+//     //         var keywordsContent = metaTags[i].getAttribute('content');
+            
+//     //         // Split the content into an array of keywords
+//     //         var keywordsArray = keywordsContent.split(',');
+            
+//     //         // Trim any leading or trailing whitespaces from each keyword
+//     //         keywordsArray = keywordsArray.map(function(keyword) {
+//     //             return keyword.trim();
+//     //         });
+            
+//     //         // Output the keywords
+//     //         // console.log('Keywords:', keywordsArray);
+            
+//     //         // You can return the keywordsArray or perform further processing here
+//     //         return keywordsArray;
+//     //     }
+//     // }
+    
+//     // Return an empty array if no keywords are found
+//     // return [];
+// }
 
-    // go to each product
-    let pageProducts = await browser.newPage();
-    for (let i = 0; i < products.length; i++) {
-      await pageProducts.goto(products[i].link);
-      await pageProducts.waitForSelector('.product');
-      let product = await pageProducts.evaluate(() => {
-        let product = {};
-        // @ts-ignore
-        product.title = document.querySelector('.product_title').innerText;
-        // @ts-ignore
-        product.price = document.querySelector('.woocommerce-Price-amount').innerText;
-        // @ts-ignore
-        product.description = document.querySelector('.woocommerce-product-details__short-description').innerText;
-        // @ts-ignore
-        product.image = document.querySelector('.woocommerce-product-gallery__image img').src;
-        return product;
-      });
-      products[i].price = product.price;
-      products[i].description = product.description;
-      products[i].image = product.image;
-    }
+// // Call the function when the DOM is ready
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Call the function to extract keywords
+//     extractKeywords();
+// });
+ 
+    });    
 
+    // console.log(result);
 
-    console.log(products);
-
+    // return result;
 		
 	}
 }
